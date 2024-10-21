26b5484a7c99604dacb4a5187c822cfe00701da6
vadlapravallika <vadlapravallika1@gmail.com>
2024-10-21 21:15:54 +0530

    updated UI changes

---
+++ toggle.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById("bar");
+const menu = document.getElementById("menu");
+if (bar) {
+  bar.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+}
